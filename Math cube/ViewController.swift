4a7f1d7f0f56e63a221640f80c213207af2a5eb8
Kuts Andrew <a.kuts@itransition.com>
2019-07-31 00:41:03 +0300

    Initial Commit

---
+++ Math cube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math cube
+//
+//  Created by Kuts, Andrey on 7/31/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
